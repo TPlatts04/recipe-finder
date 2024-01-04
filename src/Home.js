18c3b1ce771d5c3bb73cfe65fa01df30452b2837
Thomas Platts <thomasplatts2004@gmail.com>
2024-01-04 15:08:08 +0000

    Completion of home page styling and layout

--- src/Home.js
+++ src/Home.js
@@ -6,8 +6,24 @@ const Home = () => {
       <NavBar/>
       <div className="home-page-container container-fluid">
         <h1 className="home-title">
-          
+          Recipe Finder | Homepage
         </h1>
+        <div className="information">
+          <p className="inside-information">
+            <h3 className="info-title">
+              How does this Recipe Finder work from a Progammers perspective?
+            </h3>
+            <ol>
+              <li>First things first, we create the base page, which is what you see here under the name of "Homepage". <br/>This will ensure that we have the base ready to start creating our project.</li>
+              <li>Second of all we will go ahead and use a website of our choice to be able to grab recipe data as we <b>DO NOT</b><br/>want to be using an object within JavaScript to store <b>hundreds</b> potentially <b>thousands</b> of recipes within our code.<br/> The website which i have decided to use was <a target="_blank" rel="noreferrer" href="https://api-ninjas.com/api">API Ninjas.</a></li>
+              <li>Thirdly, we will implement our API key within our JavaScript code like so <code>const APIKEY = 'your API key'</code>. Make sure that<br/> you store this in an async function. You can call this variable whatever you like but this ensures that you are able to use<br/> your APIKey whenever it is needed. Furthermore, you will then have to ensure that you can get data from the Recipe<br/> website by using an Object which uses the GET method keyword (If you are using API Ninjas, then create an object<br/> following these rules: <code>method: 'GET', url: 'https://api.api-ninjas.com/v1/recipe?query=' + query, headers:  <br/>'X-Api-Key': 'YOUR_API_KEY'</code>) where you can link the given API link into a URL key, which is all given to you via<br/> the chosen webiste. <b>IF YOU NEED HELP VIEW THE WEBSITES DOCUMENTATION</b></li>
+              <li>Finally, once the the object has been created using the following rules, you can then assign a varialbe of any name,<br/> and use the await fetch (objName, APIUrl) so for example <code>const response = await fetch(ObjName, APIUrl)</code>. This will<br/> ensure that the repsonse is gathered before continuing to the next line of code. After this line you would want to<br/> declare a variable which holds the data which has been collected, in this variable use the same await and fetch<br/> method like so <code>const data = await fetch(response)</code>. Then you can go ahead and use the data as you please.</li>
+            </ol>
+          </p>
+          <h1 className="started">
+            Let's get started! Search a food you'd like the recipe for!
+          </h1>
+        </div>
       </div>
     </main>
   );
--- src/Navbar.js
+++ src/Navbar.js
@@ -2,12 +2,12 @@ const NavBar = () => {
   return ( 
     <main className="nav-wrapper container-fluid">
       <nav className="nav-container">
-        <div className="title-wrapper col-xs-4">
+        <div className="title-wrapper col-xs-2">
           <h1 className="title">Recipe Finder</h1>
         </div>
         <div className="search-wrapper col-xs-3">
           <input type="search" name="Search" id="search-bar"  className="search-bar" placeholder="Search Recipe..."/>
-          <button type="button" className="search-btn" id="search-btn"><i className="fa fa-search"></i></button>
+          <button type="button" className="search-btn" id="search-btn"><i className="fa fa-search search-icon"></i></button>
         </div>
         <div className="icon">
           <h2><i className="fa fa-utensils col-xs-4"></i></h2>
